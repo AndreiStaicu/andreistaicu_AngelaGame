8ad27bb9201aa83b45971df37bda738c6dfc6e3b
Andrei Staicu <angelloandrei.staicu@barsanti.edu.it>
2019-01-18 09:49:46 +0100

    Inizio del void loop

--- AngelaGame_StaicuAngello-Andrei/AngelaGame_StaicuAngello-Andrei.ino
+++ AngelaGame_StaicuAngello-Andrei/AngelaGame_StaicuAngello-Andrei.ino
@@ -1,11 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
 
-  int LEDRosso = 8;
-  int LEDBlu = 9;
+ // int MinusButton = 2;
+ // int OKButton = 3;
+ // int PlusButton = 4;
+
+ Serial.Begin(9600);
+ start();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int numeroMassimoScelto = 50;
+  int numeroSceltoGiocatore;
+  int numeroSceltoAngela;
+  int somma = numeroSceltoGiocatore + numeroSceltoAngela;
+  
 
+  
 }

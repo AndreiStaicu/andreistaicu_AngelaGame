0b83f3987a20b72cc770133f850768f3f1f5dbb3
Andrei Staicu <angelloandrei.staicu@barsanti.edu.it>
2019-01-25 09:52:17 +0100

    void aumentaMeta-riduciMeta e inizio partita

--- AngelaGame_StaicuAngello-Andrei/AngelaGame_StaicuAngello-Andrei.ino
+++ AngelaGame_StaicuAngello-Andrei/AngelaGame_StaicuAngello-Andrei.ino
@@ -1,21 +1,70 @@
+
+  int numeroMetaScelto = 50;
+  int numeroSceltoGiocatore;
+  int numeroSceltoAngela;
+  int somma = numeroSceltoGiocatore + numeroSceltoAngela;
+  int Giocata = 1;
+  int PrimaGiocata[7] = {0,1,2,3,4,5,6};
+  int Giocate[4];
+  int Turni = 1;
+
 void setup() {
   // put your setup code here, to run once:
-
- // int MinusButton = 2;
- // int OKButton = 3;
- // int PlusButton = 4;
-
- Serial.Begin(9600);
- start();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int numeroMassimoScelto = 50;
-  int numeroSceltoGiocatore;
-  int numeroSceltoAngela;
-  int somma = numeroSceltoGiocatore + numeroSceltoAngela;
   
+// ValoreSelezionabile();
 
+if(turni == 1)
+{
   
 }
+
+}
+void Partita()
+{
+  
+}
+
+ void AumentaMeta()
+ {
+  if(numeroMetaScelto == 99)
+  {
+    Serial.Print("Soglia massima raggunta")
+  }
+  else
+  {
+    numeroMetaScelto++;
+  }
+ }
+
+ void DiminuisciMeta()
+ {
+  if(numeroMetaScelto == 30)
+  {
+    Serial.Print("Soglia minima raggiunta")
+  }
+  else
+  {
+    numeroMetaScelto--;
+  }
+ }
+ 
+/*void ValoreSelezionabile()
+{
+  switch (Giocata)
+  {
+    case 0:
+    Giocatore = 1;
+    case 1 || 6:
+    break;
+    case 2 || 5:
+    break;
+    case 3 || 4:
+  }
+}
+*/
+
+
